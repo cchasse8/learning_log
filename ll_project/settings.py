045df5698daa08195450c3047de2d3b84d8e077b
Cooper Chasse <cchasse@mail.umw.edu>
2025-11-02 21:30:25 -0500

    Add Entry model, register Topic and Entry in admin, update settings

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -36,7 +35,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
+    "django.contrib.staticfiles","learning_logs",
 ]
 
 MIDDLEWARE = [
